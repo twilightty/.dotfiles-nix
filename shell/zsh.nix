e88171f80bc01f61ab7dc076f435d67f488f114f
Tran Trong Nghia <emladevops@protonmail.com>
2024-03-16 13:03:31 +0700

    add custom fetch tool

--- shell/zsh.nix
+++ shell/zsh.nix
@@ -7,6 +7,8 @@
         initExtraBeforeCompInit = ''
             eval "$(/opt/homebrew/bin/brew shellenv)"
             eval "$(starship init zsh)"
+
+            clear && python3 -W ignore -m twidgets
         '';
         shellAliases = {
                 nupd = "cd ~/nix-kirb && darwin-rebuild switch --flake .";
